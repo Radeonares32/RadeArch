83097f8b696472517cd631d2148dbcf2553ad112
Radeonares32 <bugra.linux.js@gmail.com>
2023-06-23 06:49:35 +0300

    iso name change

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="RadeArch"
+iso_label="RadeArch_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="RadeArch <https://github.com/Radeonares32>"
+iso_application="RadeArch"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
